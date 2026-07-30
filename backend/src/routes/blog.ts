3ada191bf5e20365f70e0fe8083760c8e5fc9043
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2026-07-30 14:43:10 +0530

    Blog route

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+
+export const blogRouter = new Hono<
+    {
+        Bindings: {
+            DATABASE_URL: string,
+            JWT_SECRET: string
+        }
+    }
+>();
+
+
+blogRouter.post('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+blogRouter.put('/a', (c) => {
+  return c.text('Hello Hono!')
+})
+
+blogRouter.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+blogRouter.get('/bulk', (c) => {
+  return c.text('Hello Hono!')
+})
